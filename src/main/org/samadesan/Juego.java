d4dea8caa52bee0f46a9daa0e0ec8b1b65383168
samadesan <samadesan@alu.edu.gva.es>
2025-04-04 13:04:29 +0200

    Aplicación con ChatGPT - 04/04/2025 - Samuel Aded Santafé

--- src/main/org/samadesan/Juego.java
+++ src/main/org/samadesan/Juego.java
@@ -71,6 +71,7 @@ public class Juego {
         } else {
             System.out.println("Personaje cargado: " + personaje.getNombre());
         }
+        this.personaje = personaje;
     }
 
     // Mostrar opciones al jugador
